1a01f52baeab68115da448decdb8cb820dce6d20
Alexa <alexandra.rojo.rivera@gmail.com>
2024-08-21 14:53:21 -0600

    Add spacer to change the height between box and second text

--- ExampleApp/app/src/main/java/com/example/exampleapp/MainActivity.kt
+++ ExampleApp/app/src/main/java/com/example/exampleapp/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -47,9 +50,33 @@ fun CertificatingCourse(nombre:String, modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         //horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Row{
+            Spacer(modifier=Modifier.height(300.dp))
+            Image(
+                painter = painterResource(id = R.drawable.logo_unam),
+                contentDescription = null,
+                modifier = modifier.size(50.dp,50.dp),
+            )
+
+            Spacer(modifier = Modifier.width(40.dp))
+            Text(
+                text = " Facultad de Ingeniería",
+                fontSize=20.sp,
+                textAlign=TextAlign.Center
+            )
+            Spacer(modifier = Modifier.width(30.dp))
+             Image(
+                   painter= painterResource(id = R.drawable.escudo_fi_color),
+                 contentDescription = null,
+                  modifier.size(50.dp, 50.dp)
+                  )
+
+        }
+
+
         Box {
 
-
+            Spacer(modifier=Modifier.height(90.dp))
             Image(
                 painter = painterResource(id = R.drawable.android_logo),
                 contentDescription = null,
@@ -64,12 +91,15 @@ fun CertificatingCourse(nombre:String, modifier: Modifier = Modifier) {
                 textAlign = TextAlign.Center,
                 modifier = modifier.fillMaxWidth()
             )
-            }
-        Spacer(modifier = modifier.height(25.dp))
+
+        }
+
+            Spacer(modifier = modifier.height(25.dp))
             Text(text = "has successfully completed" + "a 2 hours course on")
 
     }
 }
+
 @Composable
 @Preview(showBackground = true)
 fun CertificatingCoursePreview(){
